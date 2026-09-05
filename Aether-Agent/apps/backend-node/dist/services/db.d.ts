6b3eb9a5599ab4a2800adfdb537b04773ea19312
Saad Salman <154062428+SaadxSalman@users.noreply.github.com>
2026-09-05 14:55:28 +0500

    update

--- Aether-Agent/apps/backend-node/dist/services/db.d.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-declare const MONGODB_URI: string;
-export declare function connectDB(): Promise<void>;
-export declare const db: mongoose.Connection;
-export { MONGODB_URI };
-//# sourceMappingURL=db.d.ts.map
